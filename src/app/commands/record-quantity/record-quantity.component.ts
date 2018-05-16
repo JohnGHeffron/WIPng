daa91f39b0ed90ea1bc6d8488c215aecafa26512
John Heffron <jheffron@mindtool-usa.com>
2018-05-16 16:03:09 -0500

    animate reason codes hide/show

--- src/app/commands/record-quantity/record-quantity.component.ts
+++ src/app/commands/record-quantity/record-quantity.component.ts
@@ -1,11 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 import { ApiService } from '../../api.service';
 import { ReactiveFormsModule, FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-record-quantity',
   templateUrl: './record-quantity.component.html',
-  styleUrls: ['./record-quantity.component.css']
+  styleUrls: ['./record-quantity.component.css'],
+  animations: [
+    trigger('showHide', [
+      transition('void => *', [
+        style({opacity:0, transform: 'translateY(-100%)'}),
+        animate('0.3s ease-in')
+      ]),
+      transition('* => void', [
+        animate('0.3s ease-out', style({opacity:0, transform: 'translateY(-100%)'}))
+      ])
+    ])
+  ]
 })
 export class RecordQuantityComponent implements OnInit {
 
